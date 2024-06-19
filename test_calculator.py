c3e310dc543b56018ee1c884f643c0c65365936b
MarMarcz <s24566@pjwstk.edu.pl>
2024-06-19 12:22:47 +0200

    Added test that will fail

--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(multiply(-1, 5), -5)
         self.assertEqual(multiply(0, 3), 0)
         self.assertEqual(multiply(-2, -2), 4)
+        self.assertEqual(multiply(-2, -2), 1) # This test will fail
         
 if __name__ == "__main__":
     unittest.main()
